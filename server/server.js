a82f7bd10a5160cdefcaf1ecd18010c3d559d2d3
R YAMINI DEVI <131525177+YaminiReddipilli@users.noreply.github.com>
2025-12-14 11:32:04 +0530

    Update CORS configuration in server.js
    Restrict CORS to specific origin with credentials support.

--- server/server.js
+++ server/server.js
@@ -15,7 +15,7 @@ const PORT = process.env.PORT || 4000;
 const app = express();
 
 app.use(express.json());
-app.use(cors());
+app.use(cors({ origin:'https://dreampixel-texttoimagegenerator-static.onrender.com',credentials:true}));
 await connectDB();
 
 // API routes
